b5ae3d39c651fd15e51fa2ad45e63a51bef7114c
coffeemug17 <mitulpandey2@gmail.com>
2023-01-16 17:33:55 -0800

    Adding styling for the reset button, heading tags and tracking state variables defined

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 /*----- constants -----*/
-
+const COLORS = {
+    '0': 'white',
+    '1': 'purple',
+    '-1': 'green'
+};
 
 /*----- state variables -----*/
-
+let turn;//1/-1 to signify player
+let board;//Array of 9 elements
+let winner;// 1/-1 for player win, null if no winner and "T" if tie
 
 /*----- cached elements  -----*/
 
