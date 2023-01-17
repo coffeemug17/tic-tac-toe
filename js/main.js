89a59d440c035cb1cc0799d283cd1ca10d511a36
coffeemug17 <mitulpandey2@gmail.com>
2023-01-16 19:01:31 -0800

    Adding the completed render() functions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 /*----- constants -----*/
 const COLORS = {
     '0': 'white',
-    '1': 'purple',
-    '-1': 'green'
+    '1': 'green',//this is for X
+    '-1': 'blue'//this is for O
+};
+const PLAYERS = {
+    '0': '',
+    '1': 'X',
+    '-1':'O'
 };
 
 /*----- state variables -----*/
@@ -11,9 +16,52 @@ let board;//Array of 9 elements
 let winner;// 1/-1 for player win, null if no winner and "T" if tie
 
 /*----- cached elements  -----*/
+const messageEl = document.querySelector("h1");
+const resetButton = document.querySelector("button");
 
 
 /*----- event listeners -----*/
-
+resetButton.addEventListener('click',init);
 
 /*----- functions -----*/
+init();
+
+//In this function, we initialize all the state variables
+function init() {
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    winner = null;
+    turn = 1;
+    render();
+}
+
+//render() helps in visualizing elements of the DOM
+function render() {
+    renderBoard();
+    renderMessage();
+    renderControls();//Hides or shows the UI elements 
+}
+
+function renderBoard() {
+    board.forEach(function(cellVal, Idx) {
+        const cellId = `${Idx}`;
+        const cellEl = document.getElementById(cellId);
+        // cellEl.style.backgroundColor = COLORS[cellVal];
+        cellEl.innerHTML = PLAYERS[cellVal];
+    });
+
+}
+
+function renderMessage() {
+    if (winner === 'T') {
+        messageEl.innerText = "It's a tie";
+    } else if (winner) {
+        messageEl.innerHTML = `<span style = "color: ${COLORS[winner]}">${PLAYERS[winner].toUpperCase()}</span> Wins!`
+    } else {
+        messageEl.innerHTML = `<span style = "color: ${COLORS[turn]}">${PLAYERS[turn].toUpperCase()}</span>'s Turn!`
+    }
+
+}
+
+function renderControls() {
+    resetButton.style.visibility = winner ? 'visible' : 'hidden';
+}
